54dcc16cad8553feb714f3d6e96e0343ec82e182
eduardxmartinez <eduardom17809@gmail.com>
2022-01-23 19:12:39 -0700

    dev and prod config

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,25 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
+    publicPath:"/",
   },
   resolve: {
     extensions: [".js", ".jsx"],
+    alias: {
+        "@components": path.resolve(__dirname, "src/components/"),
+        "@styles": path.resolve(__dirname, "src/styles/")
+    }
   },
+  mode: "production",
   module: {
     rules: [
       {
@@ -25,17 +35,31 @@ module.exports = {
             { loader: "html-loader" }
         ],
       },
+      {
+          test: /\.s[ac]ss$/,
+          use: [
+              "style-loader",
+              "css-loader",
+              "sass-loader"
+          ]
+      }
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
         template: "./public/index.html",
         filename: "./index.html"
-    })
+    }),
+    new MiniCssExtractPlugin({
+        filename: "[name].css"
+    }),
+    new CleanWebpackPlugin(),
   ],
-  devServer: {
-    static: path.join(__dirname, "dist"),
-    compress: true,
-    port: 3006,
-  },
+  optimization: {
+      minimize: true,
+      minimizer: [
+          new CssMinimizerPlugin(),
+          new TerserPlugin(),
+      ]
+  }
 };
